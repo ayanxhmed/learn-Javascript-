2002e8bc7b90e35f951472f85c3784517a483a62
ayanxhmed <rynamd9411@gmail.com>
2025-08-15 22:56:29 +0000

    change

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = null;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33"=>33
+//"33nsk"=> NaN
+//true =>1
+//null =>0
+//undefined =>NaN
+//"nksd"=>NaN
+
+
+// study
+// https://tc39.es/ecma262/
+// https://tc39.es/ecma262/multipage/abstract-operation.html#sec-type-conversion
